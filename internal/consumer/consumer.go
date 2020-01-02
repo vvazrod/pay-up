29c29dcccf9a538c26a8f7b9a25f825e5217ecc8
Víctor Vázquez <victorvazrod@gmail.com>
2020-01-02 17:16:35 +0100

    Añade implementaciones para publicar y consumir mensajes AMQP
    Avanza las tareas #12, #13, #14 y #15

---
+++ internal/consumer/consumer.go
@@ -0,0 +1,104 @@
+package consumer
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+// Consumer of AMQP messages.
+type Consumer struct {
+	conn  *amqp.Connection
+	queue string
+	tag   string
+}
+
+// New Consumer instance.
+func New(conn *amqp.Connection, exchange, queue, tag string) (*Consumer, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't create channel. Error: %s", err.Error())
+	}
+	defer ch.Close()
+
+	if err = ch.ExchangeDeclare(
+		exchange, // name
+		"direct", // type
+		true,     // durable
+		false,    // autoDelete
+		false,    // internal
+		false,    // noWait
+		nil,      // args
+	); err != nil {
+		return nil, fmt.Errorf("Couldn't declare exchange. Error: %s", err.Error())
+	}
+
+	if _, err = ch.QueueDeclare(
+		queue, // name
+		true,  // durable
+		false, // autoDelete
+		false, // exclusive
+		false, // noWait
+		nil,   // args
+	); err != nil {
+		return nil, fmt.Errorf("Couldn't declare queue. Error: %s", err.Error())
+	}
+
+	if err = ch.QueueBind(
+		queue,    // queue name
+		queue,    // routing key
+		exchange, // exchange name
+		false,    // noWait
+		nil,      // args
+	); err != nil {
+		return nil, fmt.Errorf("Couldn't bind queue to exchange. Error: %s", err.Error())
+	}
+
+	return &Consumer{
+		conn:  conn,
+		queue: queue,
+		tag:   tag,
+	}, nil
+}
+
+// Start consuming messages with the given handler.
+//
+// Based on https://codereview.stackexchange.com/a/199894
+func (c *Consumer) Start(ctx context.Context, handle func(*amqp.Delivery)) error {
+	ch, err := c.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	msgs, err := ch.Consume(
+		c.queue, // queue name
+		c.tag,   // consumer tag
+		false,   // autoAck
+		false,   // exclusive
+		false,   // noLocal
+		false,   // noWait
+		nil,     // args
+	)
+	if err != nil {
+		return err
+	}
+
+	// Handle the messages
+	go func() {
+		select {
+		case <-ctx.Done():
+			return
+		case msg := <-msgs:
+			handle(&msg)
+		}
+	}()
+
+	// Stop the consumer when context is cancelled
+	go func() {
+		<-ctx.Done()
+		ch.Cancel(c.tag, false)
+	}()
+
+	return nil
+}
---
+++ internal/publisher/publisher.go
@@ -0,0 +1,61 @@
+package publisher
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+// Publisher of AMQP messages.
+type Publisher struct {
+	conn     *amqp.Connection
+	exchange string
+	key      string
+}
+
+// New Publisher instance.
+func New(conn *amqp.Connection, exchange, key string) (*Publisher, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't create channel. Error: %s", err.Error())
+	}
+	defer ch.Close()
+
+	if err = ch.ExchangeDeclare(
+		exchange, // name
+		"direct", // type
+		false,    // durable
+		false,    // autoDelete
+		false,    // internal
+		false,    // noWait
+		nil,      // args
+	); err != nil {
+		return nil, fmt.Errorf("Couldn't declare exchange. Error: %s", err.Error())
+	}
+
+	return &Publisher{
+		conn:     conn,
+		exchange: exchange,
+		key:      key,
+	}, nil
+}
+
+// Publish a message to the publisher's exchange with the given routing key.
+func (p *Publisher) Publish(msg *amqp.Publishing) error {
+	ch, err := p.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	if err = ch.Publish(
+		p.exchange, // exchange
+		p.key,      // key
+		false,      // mandatory
+		false,      // immediate
+		*msg,       // message
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
