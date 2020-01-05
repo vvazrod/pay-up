6a15781cb913081e8f6c388431568645c37766f5
Víctor Vázquez <victorvazrod@gmail.com>
2020-01-05 03:10:31 +0100

    Utiliza UUIDs en modelo de datos de gmicro
    Además, elimina función constructora y tests innecesarios
    
    Avanza la tarea #31

--- internal/gmicro/group/group.go
+++ internal/gmicro/group/group.go
@@ -7,16 +7,7 @@ import (
 
 // Group of people, each of which has a balance in the group.
 type Group struct {
-	ID      string          `json:"id" gorm:"type:uuid;primary_key"`
+	ID      uuid.UUID       `json:"id" gorm:"type:uuid;primary_key"`
 	Name    string          `json:"name"`
 	Members []member.Member `json:"members" gorm:"foreignkey:GroupID"`
-}
-
-// New Group instance.
-func New(name string) *Group {
-	return &Group{
-		ID:      uuid.New().String(),
-		Name:    name,
-		Members: []member.Member{},
-	}
 }
